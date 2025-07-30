1319ddd5811461c377c8a909bf0c5efd09566a9e
mahamudul404 <mahamuduljalal@gmail.com>
2025-07-30 20:34:59 +0600

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+route::get('admin/dashboard', [HomeController::class, 'index']);
